eadedfb6abcd34d7f44be514b49a31338a158311
Jim Dixon <jddixon@gmail.com>
2017-08-03 14:15:46 -0700

    rewrite setup.py, MANIFEST.in; minor edits cfg files

--- src/xlhttpd/__init__.py
+++ src/xlhttpd/__init__.py
@@ -2,7 +2,7 @@
 
 """ web server for XLattice """
 
-__version__ = '0.0.4'
-__version_date__ = '2017-06-29'
+__version__ = '0.0.5'
+__version_date__ = '2017-08-03'
 
 __all__ = ['__version__', '__version_date__']
